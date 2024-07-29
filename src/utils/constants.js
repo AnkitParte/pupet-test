f5ba1e80cc7943425b801b90ecdc4e5a005b7276
Arpita523 <arpita@aegiscovenant.com>
2024-07-29 17:29:05 +0530

    new ups

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ export const FE_URL = {
   Dev: "https://dev.dealer.aegiscovenant.com",
   Test: "https://test.dealer.aegiscovenant.com",
   Loc: "http://localhost:3000",
-  ops: "https://test.admin.aegiscovenant.com"
+  ops: "https://dev.admin.aegiscovenant.com"
 }
 
 export let testPdf =
